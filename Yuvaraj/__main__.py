557b1908882df62d5eb9b06448ce046f875ed578
Sᴏυʀαʏєɴ <157188588+Sourayen@users.noreply.github.com>
2024-03-04 15:19:38 +0530

    Ok

--- Yuvaraj/__main__.py
+++ Yuvaraj/__main__.py
@@ -2,21 +2,24 @@ import pyrogram
 import strings
 import config
 
-from Yuvaraj import bot , yuvaraj
+from Yuvaraj import bot , yuvaraj, LOGGER
 from Yuvaraj.helpers.help_func import get_datetime 
 
 async def run_clients():
-      await bot.start()
-      await yuvaraj.start()
-      await pyrogram.idle()
       zone = await get_datetime()
-      await bot.send_message(
-           chat_id=config.GROUP_ID,
-           text=strings.RESTART_TEXT1.format(date=zone["date"], time=zone["time"]))
-      await barath.send_message(
-           chat_id=config.GROUP_ID,
-           text=strings.RESTART_TEXT2.format(date=zone["date"], time=zone["time"]))
-
+      try:
+          await bot.start()
+          await yuvaraj.start()
+          await bot.send_message(
+              chat_id=config.GROUP_ID,
+              text=strings.RESTART_TEXT1.format(date=zone["date"], time=zone["time"]))
+          await barath.send_message(
+              chat_id=config.GROUP_ID,
+              text=strings.RESTART_TEXT2.format(date=zone["date"], time=zone["time"]))
+      except Exception as e:
+          LOGGER.info(e)  
+      await pyrogram.idle()
+   
 
 if __name__ == "__main__":
     yuvaraj.loop.run_until_complete(run_clients())
