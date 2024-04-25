81140e4b84523678842c3817437305686abbbb94
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-04-25 19:43:05 +0530

    Update __main__.py

--- Yuvaraj/__main__.py
+++ Yuvaraj/__main__.py
@@ -1,5 +1,4 @@
 import pyrogram
-import asyncio
 import strings
 import config
 
@@ -7,19 +6,17 @@ from Yuvaraj import bot , yuvaraj
 from Yuvaraj.helpers.help_func import get_datetime 
 
 async def run_clients():
-      zone = await get_datetime()
-      try:
-          await bot.start()
-          await yuvaraj.start()
-          await bot.send_message(
-              chat_id=config.GROUP_ID,
-              text=strings.RESTART_TEXT1.format(date=zone["date"], time=zone["time"]))
-          await barath.send_message(
-              chat_id=config.GROUP_ID,
-              text=strings.RESTART_TEXT2.format(date=zone["date"], time=zone["time"]))
-      except Exception as e:
-          LOGGER.info(e)  
+      await bot.start()
+      await yuvarj.start()
       await pyrogram.idle()
-   
+      zone = await get_datetime()
+      await bot.send_message(
+           chat_id=config.GROUP_ID,
+           text=strings.RESTART_TEXT1.format(date=zone["date"], time=zone["time"]))
+      await barath.send_message(
+           chat_id=config.GROUP_ID,
+           text=strings.RESTART_TEXT2.format(date=zone["date"], time=zone["time"]))
+
+
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(run_clients())
+    yuvaraj.loop.run_until_complete(run_clients())
