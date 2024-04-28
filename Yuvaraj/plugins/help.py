563380e7d47d05f27938dd649bba27b75e4489a6
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-04-28 22:58:11 +0530

    Update help.py

--- Yuvaraj/plugins/help.py
+++ Yuvaraj/plugins/help.py
@@ -5,36 +5,34 @@ from config import HANDLER, OWNER_ID, YUVARAJ,SOURCE
 
 @app.on_message(filters.command("help",prefixes=HANDLER) & filters.me)
 async def help_command(client, message):
-    await query.message.reply_text(plugins_list, quote=True)
-    plugins_list = """
-Yuvaraj-UserBot Plugins
-+------------+------------+
-| Autopic    | admin      |
-| afk        | animation  |
-| anime_cf   | antipm     |
-| autoscroll | broadcast  |
-| carbon     | clone      |
-| create     | dictionary |
-| dmspam     | emoji      |
-| git        | globals    |
-| google     | info       |
-| invite     | joinleave  |
-| locks      | lyrics     |
-| memify     | mention    |
-| metrics    | music      |
-| paste      | pats       |
-| ping       | profile    |
-| purge      | quotly     |
-| raid       | replyraid  |
-| restart    | sangmata   |
-| screenshot | spam       |
-| start      | stats      |
-| sticker    | stickers   |
-| sudos      | tag        |
-| tagalert   | tagall     |
-| telegraph  | text       |
-| tiny       | unsplash   |
-| upload     | vctools    |
-| vulgar     | weather    |
-+------------+------------+
-"""
+    await query.message.reply_text("Yuvaraj-UserBot Plugins\n"
+    "+------------+------------+\n"
+    "| Autopic    | admin      |\n"
+    "| afk        | animation  |\n"
+    "| anime_cf   | antipm     |\n"
+    "| autoscroll | broadcast  |\n"
+    "| carbon     | clone      |\n"
+    "| create     | dictionary |\n"
+    "| dmspam     | emoji      |\n"
+    "| git        | globals    |\n"
+    "| google     | info       |\n"
+    "| invite     | joinleave  |\n"
+    "| locks      | lyrics     |\n"
+    "| memify     | mention    |\n"
+    "| metrics    | music      |\n"
+    "| paste      | pats       |\n"
+    "| ping       | profile    |\n"
+    "| purge      | quotly     |\n"
+    "| raid       | replyraid  |\n"
+    "| restart    | sangmata   |\n"
+    "| screenshot | spam       |\n"
+    "| start      | stats      |\n"
+    "| sticker    | stickers   |\n"
+    "| sudos      | tag        |\n"
+    "| tagalert   | tagall     |\n"
+    "| telegraph  | text       |\n"
+    "| tiny       | unsplash   |\n"
+    "| upload     | vctools    |\n"
+    "| vulgar     | weather    |\n"
+    "+------------+------------+\n")
+
