a14019cc4e413c5fe58b453fbc8fe486423bc968
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-04-29 17:09:59 +0530

    Update reverse.py

--- Yuvaraj/plugins/reverse.py
+++ Yuvaraj/plugins/reverse.py
@@ -1,18 +1,20 @@
 import uuid
-from pyrogram import Client, filters
-from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, Message)
 import httpx
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from config import HANDLER
 from Yuvaraj import yuvaraj as app
-from config import HANDLER, OWNER_ID, YUVARAJ,SOURCE
 from Yuvaraj import yuvaraj as STRINGS
 
+API_URL = "https://nandha-api.onrender.com/reverse?img_url={url}"  # Replace "your_api_url_here" with your actual API URL
 COMMANDS = ['reverse', 'grs', 'pp']
 
-@app.on_message(filters.command("COMMANDS",prefixes=HANDLER) & filters.me)
+@app.on_message(filters.command(COMMANDS, prefixes=HANDLER) & filters.me)
 async def on_reverse(client: Client, message: Message) -> Message:
     if not message.reply_to_message or not message.reply_to_message.media:
         await message.reply(STRINGS.REPLY_TO_MEDIA)
         return
+
     media_type = message.reply_to_message.media
     if media_type not in ('photo', 'sticker', 'document'):
         if media_type == 'animation':
@@ -20,23 +22,28 @@ async def on_reverse(client: Client, message: Message) -> Message:
             return
         await message.reply(STRINGS.THESE_MEDIA_TYPES_ONLY)
         return
+
     status_msg = await message.reply(STRINGS.DOWNLOADING_MEDIA)
     file_path = f'downloads/{uuid.uuid4()}'
     await message.reply_to_message.download(file_path)
     await status_msg.edit(STRINGS.UPLOADING_MEDIA)
+
     async with httpx.AsyncClient(timeout=30) as async_client:
         with open(file_path, 'rb') as file:
             files = {'file': file}
             response = await async_client.post(API_URL, files=files)
+
         response_json = response.json()
         if response.status_code != 200:
             await message.reply(STRINGS.API_ERROR.format(response_json['error']) + STRINGS.SUPPORT_CHAT)
             return
+
         search_keyword = response_json['keyword']
         url = response_json['url']
+
     text = STRINGS.REVERSE_RESULT.format(search_keyword, url)
     reply_markup = [
         [InlineKeyboardButton('Open Link', url=url)]
-        ]
+    ]
     await message.reply(text, reply_markup=InlineKeyboardMarkup(reply_markup))
     await status_msg.delete()
