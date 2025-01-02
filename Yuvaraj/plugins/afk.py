d63ce9c72287673f16bbd0ac446aebab641ac8be
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2025-01-02 21:15:56 +0530

    Update afk.py

--- Yuvaraj/plugins/afk.py
+++ Yuvaraj/plugins/afk.py
@@ -1,90 +1,45 @@
 from pyrogram import filters
-import asyncio
-from pyrogram import client
 from Yuvaraj import yuvaraj
-from Yuvaraj.helpers.help_func import get_arg
-import Yuvaraj.yuvaraj_db.afk_db as Zect
-from Yuvaraj.helpers.help_func import user_afk
-from Yuvaraj import get_readable_time
-from Yuvaraj.helpers.utils import get_message_type, Types
 from config import HANDLER, OWNER_ID, GROUP_ID
-from Yuvaraj.helpers.help_func import get_datetime 
-import time
+from Restart import restart_program
+import os
+import re
+from datetime import datetime
+from Yuvaraj.yuvaraj_db.afk_db import *
 
+BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False
 
-MENTIONED = []
-AFK_RESTIRECT = {}
-DELAY_TIME = 20
+async def afk_remove(_, client, update):
+    if await GET_AFK():
+        Busy_time = await GET_AFK_TIME()
+        formatted_elapsed_time = calculate_time(Busy_time, datetime.now())
+        await UNSET_AFK()
+        await update.reply_text(f"➲ **Hello**, Master Welcome Again ✨🥀.\n➲ **Your Offline Duration**: `{formatted_elapsed_time}`🥺")
+        return False
 
+def calculate_time(start_time, end_time):
+    ping_time = (end_time - start_time).total_seconds() * 1000
+    uptime = (end_time - start_time).total_seconds()
+    hours, remainder = divmod(uptime, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    END = f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
+    return END
 
-@yuvaraj.on_message(filters.command("afk", HANDLER) & filters.me)
-async def afk(yuvaraj, message):
-    afk_time = int(time.time())
-    arg = get_arg(message)
-    if not arg:
-        reason = None
+@yuvaraj.on_message(filters.command(["busy", "offline", "afk"], prefixes=HANDLER) & filters.user(OWNER_ID))
+async def set_afk(_, message):
+    Busy_time = datetime.now()
+    if len(message.command) < 2:
+        await SET_AFK(Busy_time, None)
+        await message.reply_text("➲ Successfuly set you in afk!")
     else:
-        reason = arg
-    await Zect.set_afk(True, afk_time, reason)
-    await message.edit("**I'm goin' AFK**")
+        Reason_Of_Busy = " ".join(message.command[1:])
+        await SET_AFK(Busy_time, Reason_Of_Busy)
+        await message.reply_text(f"➲ Successfuly set you in afk!")
+    
+    
+@yuvaraj.on_message(filters.user(OWNER_ID) & filters.create(afk_remove))
+async def remove_busy_mode(_, message):
+    pass
 
-
-@yuvaraj.on_message(filters.mentioned & ~filters.bot & filters.create(user_afk), group=11)
-async def afk_mentioned(_, message):
-    global MENTIONED
-    afk_time, reason = await Zect.afk_stuff()
-    afk_since = get_readable_time(time.time() - afk_time)
-    if "-" in str(message.chat.id):
-        cid = str(message.chat.id)[4:]
-    else:
-        cid = str(message.chat.id)
-
-    if cid in list(AFK_RESTIRECT) and int(AFK_RESTIRECT[cid]) >= int(time.time()):
-        return
-    AFK_RESTIRECT[cid] = int(time.time()) + DELAY_TIME
-    if reason:
-       await message.reply(
-        f"**I'm AFK right now (since {afk_since})\nReason:** __{reason}__"
-        )
-    else:
-        await message.reply(f"**I'm AFK right now (since {afk_since})**")
-
-        _, message_type = get_message_type(message)
-        if message_type == Types.TEXT:
-            text = message.text or message.caption
-        else:
-            text = message_type.name
-
-        MENTIONED.append(
-            {
-                "user": message.from_user.first_name,
-                "user_id": message.from_user.id,
-                "chat": message.chat.title,
-                "chat_id": cid,
-                "text": text,
-                "message_id": message.message_id,
-            }
-        )
-
-
-@yuvaraj.on_message(filters.create(user_afk) & filters.outgoing)
-async def auto_unafk(_, message):
-    await Zect.set_unafk()
-    unafk_message = await barath.send_message(message.chat.id, "**I'm no longer AFK**")
-    global MENTIONED
-    text = "**Total {} mentioned you**\n".format(len(MENTIONED))
-    for x in MENTIONED:
-        msg_text = x["text"]
-        if len(msg_text) >= 11:
-            msg_text = "{}...".format(x["text"])
-        text += "- [{}](https://t.me/c/{}/{}) ({}): {}\n".format(
-            x["user"],
-            x["chat_id"],
-            x["message_id"],
-            x["chat"],
-            msg_text,
-        )
-        await yuvaraj.send_message(GROUP_ID, text)
-        MENTIONED = []
-    await asyncio.sleep(2)
-    await unafk_message.delete()
+MOD_NAME = 'Afk'
+MOD_HELP = ".afk <reason (optional)> - To set you in afk, if anyone dm you they get alert!"
