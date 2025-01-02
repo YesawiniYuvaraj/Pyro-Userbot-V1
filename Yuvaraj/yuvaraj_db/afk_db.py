50488e041fb1b3d2b3e7a8c12da794bdac79ef27
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2025-01-02 21:15:13 +0530

    Update afk_db.py

--- Yuvaraj/yuvaraj_db/afk_db.py
+++ Yuvaraj/yuvaraj_db/afk_db.py
@@ -1,20 +1,20 @@
-from Yuvaraj import db, DATABASE
+from Yuvaraj import db
 import asyncio
 
-db = DATABASE["afk"]
+bd = db["afk"]
 
 async def SET_AFK(time, reason):
     doc = {"_id": 1, "stats": True, "time": time, "reason": reason}
     try:
-        await db.insert_one(doc)
+        await bd.insert_one(doc)
     except Exception:
-        await db.update_one({"_id": 1}, {"$set": {"stats": True, "time": time, "reason": reason}})
+        await bd.update_one({"_id": 1}, {"$set": {"stats": True, "time": time, "reason": reason}})
         
 async def UNSET_AFK():
-    await db.update_one({"_id": 1}, {"$set": {"stats": False, "time": None, "reason": None}})
+    await bd.update_one({"_id": 1}, {"$set": {"stats": False, "time": None, "reason": None}})
     
 async def GET_AFK():
-    Find = await db.find_one({"_id": 1})
+    Find = await bd.find_one({"_id": 1})
     if not Find:
         return False
     else:
@@ -22,7 +22,7 @@ async def GET_AFK():
         return stats
 
 async def GET_AFK_TIME():
-    Find = await db.find_one({"_id": 1})
+    Find = await bd.find_one({"_id": 1})
     if not Find:
         return False
     else:
@@ -30,7 +30,7 @@ async def GET_AFK_TIME():
         return value
 
 async def GET_AFK_REASON():
-    Find = await db.find_one({"_id": 1})
+    Find = await bd.find_one({"_id": 1})
     if not Find:
         return False
     else:
