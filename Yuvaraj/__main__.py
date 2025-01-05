f761b959f41c9421a404915cb51dbdf627991a69
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2025-01-05 22:07:56 +0530

    Update __main__.py

--- Yuvaraj/__main__.py
+++ Yuvaraj/__main__.py
@@ -16,13 +16,21 @@ logging.basicConfig(
 
 log = logging.getLogger(__name__)
 
+PORT = int(os.environ.get("PORT", 8080))
+BIND_ADDRESS = str(os.environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
 
-async def start_services():
-    server = web.AppRunner(web_server())
-    await server.setup()
-    await web.TCPSite(server, "0.0.0.0", 8080).start()
-    logging.info("Web server initialized successfully.")
 
+async def start_services():        
+        server = web.AppRunner(web_server())
+        await server.setup()
+        await web.TCPSite(server, BIND_ADDRESS, PORT).start()
+        log.info("Web Server Initialized Successfully")
+        log.info("=========== Service Startup Complete ===========")
+  
+        asyncio.create_task(keep_alive())
+        log.info("Keep Alive Service Started")
+        log.info("=========== Initializing Web Server ===========")
+        
 
 async def run_clients():
 
