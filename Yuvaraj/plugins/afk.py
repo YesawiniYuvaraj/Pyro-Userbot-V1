7e9ea3b47e0bfcdadefe5f81d40f1cc9535b8674
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2025-01-05 22:17:28 +0530

    Update afk.py

--- Yuvaraj/plugins/afk.py
+++ Yuvaraj/plugins/afk.py
@@ -5,7 +5,7 @@ from Restart import restart_program
 import os
 import re
 from datetime import datetime
-from Yuvaraj.yuvaraj_db.afk_db import *
+from Yuvaraj.yuvaraj_db.afk_db import SET_AFK, UNSET_AFK, GET_AFK_TIME, GET_AFK_REASON
 
 BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False
 
